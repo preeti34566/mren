13447196c2664de998f69294e6da348340ada541
Preeti <sharmaaglina@gmail.com>
2023-06-02 11:47:14 +0530

    Remove all folders

--- src/Service/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// const usersUrl = 'http://localhost:3000';
- const usersUrl = 'http://localhost:8000';
-
-export const addUser = async (user) => {
-    return await axios.post(`${usersUrl}/add`, user);
-}
